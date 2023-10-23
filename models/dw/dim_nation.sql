fc3c3f2fc58d90a3d25b226835118205351227c0
carlos.gonzalezl@softtek.com <carlos.gonzalezl@softtek.com>
2023-10-23 20:27:52 +0000

    Committing changes

--- models/dw/dim_nation.sql
+++ models/dw/dim_nation.sql
@@ -1,11 +1,29 @@
 {{
     config(
         materialized = 'incremental',
-        unique_key = 'n_nationkey'
+        unique_key = 'nation_id'
     )
 }}
 
-select * from {{source('tpch_sf100','nation')}}
-{% if is_incremental() %}
-    where n_nationkey not in (select n_nationkey from {{this}})
-{% endif %}
+WITH FINAL AS(
+    SELECT
+        md5(nat.n_nationkey)                            as nation_id,
+        nat.n_nationkey                                 as nation_src_num,
+        md5(nat.n_regionkey)                            as region_id,
+        nat.n_regionkey                                 as region_src_num,
+
+        nat.n_name                                      as nation_name,
+        nat.n_comment                                   as nation_comment,
+        {% if is_incremental() %}
+        nvl(this.audt_create_date, current_timestamp()) as audt_create_date,
+        {% else %}
+        current_timestamp()                             as audt_create_date,
+        {% endif %}
+        current_timestamp()                             as audt_update_date
+    FROM {{ref("tpch_sf100_nation")}} nat
+    {% if is_incremental() %}
+    LEFT JOIN {{this}} as this on nat.n_nationkey = this.nation_src_num
+    {% endif %}
+    WHERE nat.dbt_valid_to is null
+)
+SELECT * FROM FINAL
