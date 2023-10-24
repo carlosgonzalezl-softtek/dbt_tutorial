344c734ff089f19503cab87df9bb01a12b9bc49b
carlos.gonzalezl@softtek.com <carlos.gonzalezl@softtek.com>
2023-10-24 20:45:53 +0000

    Committing changes

--- models/dw/dim_customer.sql
+++
@@ -1,7 +0,0 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
-
-SELECT * FROM {{source("tpch_sf100","customer")}}
--- models/dw/sample_model.sql
+++
@@ -1,6 +0,0 @@
-{{
-    config(
-        materialized = 'view'
-    )
-}}
-select 1 as column1 from dual
